b3641493bf923bb525e2ec11c7d9897f7f0c4a30
Fernando <fernando22future@gmail.com>
2020-04-13 15:27:32 -0300

    Versão final 1.0

--- clima.js
+++ clima.js
@@ -2,21 +2,26 @@ var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
+var pressao = document.querySelector('.pressao')
+var umidade = document.querySelector('.umidade')
 var button = document.querySelector('.submit');
 
 
 button.addEventListener('click', function(name) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&units=metric&appid=50a7aa80fa492fa92e874d23ad061374')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&units=metric&appid=9a4afdb555b5bbbe28fbdd8df81ecccc&lang=pt')
         .then(response => response.json())
         .then(data => {
             var tempValue = data['main']['temp'];
             var nameValue = data['name'];
             var descValue = data['weather'][0]['description'];
+            var pressaoValue = data['main']['pressure']
+            var umidadeValue = data['main']['humidity']
 
             main.innerHTML = nameValue;
-            desc.innerHTML = "Descrição - " + descValue;
-            temp.innerHTML = "Temperatura - " + tempValue + '°C';
+            desc.innerHTML = "Nebulosidade: " + descValue;
+            temp.innerHTML = "Temperatura: " + tempValue + '°C';
+            pressao.innerHTML = 'Pressão atmosférica: ' + pressaoValue + ' hPa'
+            umidade.innerHTML = 'Umidade: ' + umidadeValue + '%'
             input.value = "";
 
         })
